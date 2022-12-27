24e063b2f320a56e78a3517a1984b90969035c53
Paul Serre <paul27000@gmail.com>
2022-12-26 21:32:51 -0300

    feat(footer): Add google map

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,11 +1,20 @@
-import { HStack, VStack, useColorModeValue, Link } from "@chakra-ui/react";
+import { HStack, VStack, useColorModeValue, Link, Box, Text } from "@chakra-ui/react";
+import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
 import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
+import AddressIcon from "./icons/Address";
 import FacebookRounded from "./icons/FacebookRounded";
 import InstagramRounded from "./icons/InstagramRounded";
 import Logo from "./icons/Logo";
+import MailIcon from "./icons/Mail";
+import PhoneIcon from "./icons/Phone";
+import WhatsappIcon from "./icons/Whatsapp";
 
 export default function Footer() {
+
+  const { isLoaded } = useLoadScript({ googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY });
+
+  const center = useMemo(() => ({lat: -46.44526533318304, lng: -67.5206430306525}), [])
 
     const { t } = useTranslation();
 
@@ -23,20 +32,24 @@ export default function Footer() {
           href: '/returns'
         },
         {
-            label: t('common:footer.terms_conditions'),
-            href: '/terms'
+          label: t('common:footer.terms_conditions'),
+          href: '/terms'
         },
         {
-            label: t('common:footer.payment_methods'),
-            href: '/payment'
+          label: t('common:footer.payment_methods'),
+          href: '/payment'
         }
       ]), [t]);
 
     return (
-        <HStack bg={useColorModeValue('gray.100', 'gray.900')}  px={{ base: 0, lg: 14}}>
-            <VStack>
-                <Logo boxSize={{ base: 16, lg: 28}}/>
-                <HStack>
+        <HStack 
+          bg={useColorModeValue('gray.100', 'gray.900')} 
+          px={{ base: 0, lg: 6}}
+          py={14}
+        >
+            <VStack alignItems="center">
+                <Logo boxSize={{ base: 28, lg: 52}}/>
+                <HStack spacing={4} alignItems="center">
                     <FacebookRounded 
                       boxSize={{ base: 4, lg: 7}}
                       _hover={{ transform: 'scale(1.2)', transition: 'all .3s ease-in-out' }}
@@ -47,7 +60,7 @@ export default function Footer() {
                     />
                 </HStack>
             </VStack>
-            <VStack>
+            <VStack alignItems='flex-start'>
             {Links.map(({ label, href }) => (
               <Link 
                 key={label} 
@@ -56,6 +69,28 @@ export default function Footer() {
                 cursor='pointer'
               >{label.toUpperCase()}</Link>
             ))}
+            </VStack>
+            <VStack alignItems='flex-start' spacing="3">
+                <HStack>
+                  <PhoneIcon boxSize={6}/>
+                  <Text>(0297) 4830778</Text>
+                </HStack>
+                <HStack>
+                  <WhatsappIcon boxSize={6}/>
+                  <Text>+54 9 2974089060</Text>
+                </HStack>
+                <HStack>
+                  <MailIcon boxSize={6}/>
+                  <Text>byndeco@yahoo.com.ar</Text>
+                </HStack>
+                <HStack>
+                  <AddressIcon boxSize={6}/>
+                  <Text overflowWrap='break-word'>Progreso lópez 22 (esquina Av. Independencia) Caleta Olivia- Santa Cruz</Text>
+                </HStack>
+              { !isLoaded ? <div>Loading</div> :
+                <GoogleMap zoom={15} center={center} mapContainerStyle={{ width: '400px', height: '200px' }}>
+                  <MarkerF position={center}/>
+                </GoogleMap>}
             </VStack>
         </HStack>
     )
